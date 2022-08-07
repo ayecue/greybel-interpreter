53bd244c6f3bb70da3c2444a7c50aebc7ac7703f
ayecue <soerenwehmeier@googlemail.com>
2022-08-07 16:21:12 +0200

    1.0.2 support possible return arg being null

--- src/operations/return.ts
+++ src/operations/return.ts
@@ -4,6 +4,7 @@ import context from '../context';
 import Defaults from '../types/default';
 import { CustomValue } from '../types/generics';
 import Operation, { CPSVisit } from './operation';
+import Reference from './reference';
 
 export default class Return extends Operation {
   readonly item: ASTReturnStatement;
@@ -15,7 +16,11 @@ export default class Return extends Operation {
   }
 
   async build(visit: CPSVisit): Promise<Operation> {
-    this.arg = await visit(this.item.argument);
+    if (this.item.argument) {
+      this.arg = await visit(this.item.argument);
+    } else {
+      this.arg = new Reference(Defaults.Void);
+    }
     return this;
   }
 
