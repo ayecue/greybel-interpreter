f97645074e553740a7cc4e848526b196c66569b9
ayecue <soerenwehmeier@googlemail.com>
2022-08-11 20:17:35 +0200

    1.1.0 fix showing invalid import targets

--- src/cps.ts
+++ src/cps.ts
@@ -44,7 +44,7 @@ import NegatedBinary from './operations/negated-binary';
 import NewInstance from './operations/new-instance';
 import Noop from './operations/noop';
 import Not from './operations/not';
-import Operation from './operations/operation';
+import Operation, { CPSVisit } from './operations/operation';
 import Resolve from './operations/resolve';
 import Return from './operations/return';
 import While from './operations/while';
@@ -52,194 +52,182 @@ import While from './operations/while';
 export class CPSContext {
   readonly target: string;
   readonly handler: HandlerContainer;
-  currentTarget: string;
 
-  constructor(
-    target: string,
-    handler: HandlerContainer,
-    currentTarget: string = null
-  ) {
+  constructor(target: string, handler: HandlerContainer) {
     this.target = target;
     this.handler = handler;
-    this.currentTarget = currentTarget;
-  }
-
-  getCurrentTarget(): string {
-    return this.currentTarget;
   }
 }
 
+const visit = async (
+  context: CPSContext,
+  currentTarget: string,
+  item: ASTBase
+): Promise<Operation> => {
+  const defaultVisit = visit.bind(null, context, currentTarget);
+
+  switch (item.type) {
+    case ASTType.MapConstructorExpression:
+      return new MapOperation(
+        item as ASTMapConstructorExpression,
+        currentTarget
+      ).build(defaultVisit);
+    case ASTType.ListConstructorExpression:
+      return new List(
+        item as ASTListConstructorExpression,
+        currentTarget
+      ).build(defaultVisit);
+    case ASTType.AssignmentStatement:
+      return new Assign(item as ASTAssignmentStatement, currentTarget).build(
+        defaultVisit
+      );
+    case ASTType.MemberExpression:
+    case ASTType.Identifier:
+    case ASTType.IndexExpression:
+      return new Resolve(item, currentTarget).build(defaultVisit);
+    case ASTType.FunctionDeclaration:
+      return new FunctionOperation(
+        item as ASTFunctionStatement,
+        currentTarget
+      ).build(defaultVisit);
+    case ASTType.InvalidCodeExpression:
+      return new Noop(item).build(defaultVisit);
+    case ASTType.WhileStatement:
+      return new While(item as ASTWhileStatement, currentTarget).build(
+        defaultVisit
+      );
+    case ASTType.ForGenericStatement:
+      return new For(item as ASTForGenericStatement, currentTarget).build(
+        defaultVisit
+      );
+    case ASTType.IfStatement:
+    case ASTType.IfShortcutStatement:
+      return new IfStatement(item as ASTIfStatement, currentTarget).build(
+        defaultVisit
+      );
+    case ASTType.ReturnStatement:
+      return new Return(item as ASTReturnStatement, currentTarget).build(
+        defaultVisit
+      );
+    case ASTType.BreakStatement:
+      return new Break(item, currentTarget).build(defaultVisit);
+    case ASTType.ContinueStatement:
+      return new Continue(item, currentTarget).build(defaultVisit);
+    case ASTType.CallExpression:
+      return new Call(item as ASTCallExpression, currentTarget).build(
+        defaultVisit
+      );
+    case ASTType.CallStatement:
+      return defaultVisit((item as ASTCallStatement).expression);
+    case ASTTypeExtended.FeatureImportExpression: {
+      const importExpr = item as ASTFeatureImportExpression;
+      const target = await context.handler.resourceHandler.getTargetRelativeTo(
+        currentTarget,
+        importExpr.path
+      );
+      const code = await context.handler.resourceHandler.get(target);
+
+      if (code == null) {
+        throw new Error(`Cannot find import ${currentTarget}.`);
+      }
+
+      const subVisit = visit.bind(null, context, target);
+      const importStatement = await new Import(importExpr, target, code).build(
+        subVisit
+      );
+
+      return importStatement;
+    }
+    case ASTTypeExtended.FeatureIncludeExpression: {
+      const includeExpr = item as ASTFeatureIncludeExpression;
+      const target = await context.handler.resourceHandler.getTargetRelativeTo(
+        currentTarget,
+        includeExpr.path
+      );
+      const code = await context.handler.resourceHandler.get(target);
+
+      if (code == null) {
+        throw new Error(`Cannot find include ${currentTarget}.`);
+      }
+
+      const subVisit = visit.bind(null, context, target);
+      const importStatement = await new Include(
+        includeExpr,
+        target,
+        code
+      ).build(subVisit);
+
+      return importStatement;
+    }
+    case ASTType.ImportCodeExpression: {
+      const importExpr = item as ASTImportCodeExpression;
+      const target = await context.handler.resourceHandler.getTargetRelativeTo(
+        currentTarget,
+        importExpr.fileSystemDirectory
+      );
+      const code = await context.handler.resourceHandler.get(target);
+
+      if (code == null) {
+        throw new Error(`Cannot find native import ${currentTarget}.`);
+      }
+
+      const subVisit = visit.bind(null, context, target);
+      const importStatement = await new Include(importExpr, target, code).build(
+        subVisit
+      );
+
+      return importStatement;
+    }
+    case ASTTypeExtended.FeatureDebuggerExpression:
+      return new DebuggerStatement(item, currentTarget);
+    case ASTType.BooleanLiteral:
+    case ASTType.StringLiteral:
+    case ASTType.NumericLiteral:
+    case ASTType.NilLiteral:
+      return new Literal(item as ASTLiteral, currentTarget).build(defaultVisit);
+    case ASTType.EmptyExpression:
+      return new Noop(item, currentTarget).build(defaultVisit);
+    case ASTType.BinaryExpression:
+    case ASTType.LogicalExpression:
+      return new Evaluate(item as ASTEvaluationExpression, currentTarget).build(
+        defaultVisit
+      );
+    case ASTType.NegationExpression:
+      return new Not(item as ASTUnaryExpression, currentTarget).build(
+        defaultVisit
+      );
+    case ASTType.BinaryNegatedExpression:
+      return new NegatedBinary(item as ASTUnaryExpression, currentTarget).build(
+        defaultVisit
+      );
+    case ASTType.UnaryExpression: {
+      const unaryExpr = item as ASTUnaryExpression;
+
+      if (unaryExpr.operator === 'new') {
+        return new NewInstance(unaryExpr).build(defaultVisit);
+      } else if (unaryExpr.operator === '@') {
+        return new FunctionReference(unaryExpr).build(defaultVisit);
+      }
+
+      throw new Error('Unknown unary expression.');
+    }
+    case ASTType.Chunk:
+      return new Chunk(item as ASTChunk, currentTarget).build(defaultVisit);
+    default:
+      throw new Error(`Unexpected AST type ${item.type}.`);
+  }
+};
+
 export default class CPS {
   private readonly context: CPSContext;
+  private __visit: CPSVisit;
 
   constructor(context: CPSContext) {
     this.context = context;
+    this.__visit = visit.bind(null, context, context.target);
   }
 
-  async visit(item: ASTBase): Promise<Operation> {
-    const context = this.context;
-    const visit = this.visit.bind(this);
-
-    switch (item.type) {
-      case ASTType.MapConstructorExpression:
-        return new MapOperation(
-          item as ASTMapConstructorExpression,
-          context.target
-        ).build(visit);
-      case ASTType.ListConstructorExpression:
-        return new List(
-          item as ASTListConstructorExpression,
-          context.target
-        ).build(visit);
-      case ASTType.AssignmentStatement:
-        return new Assign(item as ASTAssignmentStatement, context.target).build(
-          visit
-        );
-      case ASTType.MemberExpression:
-      case ASTType.Identifier:
-      case ASTType.IndexExpression:
-        return new Resolve(item, context.target).build(visit);
-      case ASTType.FunctionDeclaration:
-        return new FunctionOperation(
-          item as ASTFunctionStatement,
-          context.target
-        ).build(visit);
-      case ASTType.InvalidCodeExpression:
-        return new Noop(item).build(visit);
-      case ASTType.WhileStatement:
-        return new While(item as ASTWhileStatement, context.target).build(
-          visit
-        );
-      case ASTType.ForGenericStatement:
-        return new For(item as ASTForGenericStatement, context.target).build(
-          visit
-        );
-      case ASTType.IfStatement:
-      case ASTType.IfShortcutStatement:
-        return new IfStatement(item as ASTIfStatement, context.target).build(
-          visit
-        );
-      case ASTType.ReturnStatement:
-        return new Return(item as ASTReturnStatement, context.target).build(
-          visit
-        );
-      case ASTType.BreakStatement:
-        return new Break(item, context.target).build(visit);
-      case ASTType.ContinueStatement:
-        return new Continue(item, context.target).build(visit);
-      case ASTType.CallExpression:
-        return new Call(item as ASTCallExpression, context.target).build(visit);
-      case ASTType.CallStatement:
-        return visit((item as ASTCallStatement).expression);
-      case ASTTypeExtended.FeatureImportExpression: {
-        const importExpr = item as ASTFeatureImportExpression;
-        const target =
-          await context.handler.resourceHandler.getTargetRelativeTo(
-            context.target,
-            importExpr.path
-          );
-        const code = await context.handler.resourceHandler.get(target);
-
-        if (code == null) {
-          throw new Error(`Cannot find import ${context.target}.`);
-        }
-
-        context.currentTarget = target;
-
-        const importStatement = await new Import(
-          importExpr,
-          target,
-          code
-        ).build(visit);
-        context.currentTarget = context.target;
-
-        return importStatement;
-      }
-      case ASTTypeExtended.FeatureIncludeExpression: {
-        const includeExpr = item as ASTFeatureIncludeExpression;
-        const target =
-          await context.handler.resourceHandler.getTargetRelativeTo(
-            context.target,
-            includeExpr.path
-          );
-        const code = await context.handler.resourceHandler.get(target);
-
-        if (code == null) {
-          throw new Error(`Cannot find include ${context.target}.`);
-        }
-
-        context.currentTarget = target;
-
-        const importStatement = await new Include(
-          includeExpr,
-          target,
-          code
-        ).build(visit);
-        context.currentTarget = context.target;
-
-        return importStatement;
-      }
-      case ASTType.ImportCodeExpression: {
-        const importExpr = item as ASTImportCodeExpression;
-        const target =
-          await context.handler.resourceHandler.getTargetRelativeTo(
-            context.target,
-            importExpr.fileSystemDirectory
-          );
-        const code = await context.handler.resourceHandler.get(target);
-
-        if (code == null) {
-          throw new Error(`Cannot find native import ${context.target}.`);
-        }
-
-        context.currentTarget = target;
-
-        const importStatement = await new Include(
-          importExpr,
-          target,
-          code
-        ).build(visit);
-        context.currentTarget = context.target;
-
-        return importStatement;
-      }
-      case ASTTypeExtended.FeatureDebuggerExpression:
-        return new DebuggerStatement(item, context.target);
-      case ASTType.BooleanLiteral:
-      case ASTType.StringLiteral:
-      case ASTType.NumericLiteral:
-      case ASTType.NilLiteral:
-        return new Literal(item as ASTLiteral, context.target).build(visit);
-      case ASTType.EmptyExpression:
-        return new Noop(item, context.target).build(visit);
-      case ASTType.BinaryExpression:
-      case ASTType.LogicalExpression:
-        return new Evaluate(
-          item as ASTEvaluationExpression,
-          context.target
-        ).build(visit);
-      case ASTType.NegationExpression:
-        return new Not(item as ASTUnaryExpression, context.target).build(visit);
-      case ASTType.BinaryNegatedExpression:
-        return new NegatedBinary(
-          item as ASTUnaryExpression,
-          context.target
-        ).build(visit);
-      case ASTType.UnaryExpression: {
-        const unaryExpr = item as ASTUnaryExpression;
-
-        if (unaryExpr.operator === 'new') {
-          return new NewInstance(unaryExpr).build(visit);
-        } else if (unaryExpr.operator === '@') {
-          return new FunctionReference(unaryExpr).build(visit);
-        }
-
-        throw new Error('Unknown unary expression.');
-      }
-      case ASTType.Chunk:
-        return new Chunk(item as ASTChunk, context.target).build(visit);
-      default:
-        throw new Error(`Unexpected AST type ${item.type}.`);
-    }
+  visit(item: ASTBase): Promise<Operation> {
+    return this.__visit(item);
   }
 }
