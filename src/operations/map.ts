492ff47b753f07673cf5a5b2a27b684fc469fe2d
ayecue <soerenwehmeier@googlemail.com>
2022-08-30 19:29:02 +0200

    1.1.2 implement core version which is supporting any type as key in map

--- src/operations/map.ts
+++ src/operations/map.ts
@@ -3,12 +3,11 @@ import { ASTMapConstructorExpression, ASTMapKeyString } from 'greyscript-core';
 import context from '../context';
 import { CustomValue } from '../types/generics';
 import CustomMap from '../types/map';
-import CustomString from '../types/string';
 import Operation, { CPSVisit } from './operation';
 
 export default class MapOperation extends Operation {
   readonly item: ASTMapConstructorExpression;
-  fields: Map<string, Operation>;
+  fields: Map<Operation, Operation>;
 
   constructor(item: ASTMapConstructorExpression, target?: string) {
     super(null, target);
@@ -16,10 +15,10 @@ export default class MapOperation extends Operation {
   }
 
   async build(visit: CPSVisit): Promise<Operation> {
-    this.fields = new Map<string, Operation>();
+    this.fields = new Map<Operation, Operation>();
     const defers = this.item.fields.map(async (child) => {
       const mapKeyString = child as ASTMapKeyString;
-      this.fields.set(mapKeyString.key, await visit(mapKeyString.value));
+      this.fields.set(await visit(mapKeyString.key), await visit(mapKeyString.value));
     });
     await Promise.all(defers);
     return this;
@@ -29,7 +28,7 @@ export default class MapOperation extends Operation {
     const newMap = new Map<CustomValue, CustomValue>();
 
     for (const [key, value] of this.fields) {
-      newMap.set(new CustomString(key), await value.handle(ctx));
+      newMap.set(await key.handle(ctx), await value.handle(ctx));
     }
 
     return new CustomMap(newMap);
