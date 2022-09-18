6f96d1aabb827ba146731ea457cd8a30dd3cba06
ayecue <soerenwehmeier@googlemail.com>
2022-09-18 13:43:40 +0200

    1.2.5 add wait for input/keypress

--- src/handler/output.ts
+++ src/handler/output.ts
@@ -1,6 +1,8 @@
 export abstract class OutputHandler {
   abstract print(message: string): void;
   abstract progress(timeout: number): Promise<void>;
+  abstract waitForInput(): Promise<string>;
+  abstract waitForKeyPress(): Promise<number>;
 }
 
 export class DefaultOutputHandler extends OutputHandler {
@@ -12,5 +14,13 @@ export class DefaultOutputHandler extends OutputHandler {
     return new Promise((resolve, _reject) => {
       setTimeout(resolve, timeout);
     });
+  }
+
+  waitForInput(): Promise<string> {
+    return Promise.resolve('test');
+  }
+
+  waitForKeyPress(): Promise<number> {
+    return Promise.resolve(13);
   }
 }
