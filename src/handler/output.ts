3ee4736a137269dd4808282c25145e2c16d4b17b
ayecue <soerenwehmeier@googlemail.com>
2022-09-18 04:16:06 +0200

    1.2.4 add progress functionality in output

--- src/handler/output.ts
+++ src/handler/output.ts
@@ -1,9 +1,16 @@
 export abstract class OutputHandler {
   abstract print(message: string): void;
+  abstract progress(timeout: number): Promise<void>;
 }
 
 export class DefaultOutputHandler extends OutputHandler {
   print(message: string) {
     console.log(message);
+  }
+
+  progress(timeout: number): Promise<void> {
+    return new Promise((resolve, _reject) => {
+      setTimeout(resolve, timeout);
+    });
   }
 }
