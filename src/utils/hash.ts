f5ad9b2301472d9c7df0be09e1e244582cc1d1ac
ayecue <soerenwehmeier@googlemail.com>
2024-12-09 21:44:57 +0100

    5.2.1 use imurmurhash for string hashing

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,3 +1,5 @@
+import MurmurHash3 from 'imurmurhash';
+
 export function rotateBits(n: number) {
   return (n >> 1) | (n << 31);
 }
@@ -10,27 +12,6 @@ export function getHashCode(value: number, offset: number = 0): number {
   return ((offset << 5) - offset + unsigned) | 0;
 }
 
-export const getStringHashCode = (function () {
-  const cache = new Map<string, number>();
-  const generateHash = (s: string) => {
-    for (var i = 0, h = 0; i < s.length; i++)
-      h = (Math.imul(31, h) + s.charCodeAt(i)) | 0;
-    return h;
-  };
-
-  return (value: string): number => {
-    if (value.length === 0) {
-      return 0;
-    }
-
-    const cachedHash = cache.get(value);
-
-    if (cachedHash !== undefined) {
-      return cachedHash;
-    }
-
-    const hash = generateHash(value);
-    cache.set(value, hash);
-    return hash;
-  };
-})();
+export const getStringHashCode = (str: string) => {
+  return MurmurHash3(str).result();
+}
